8aff9bddef4a3f6e9ec6042e1f773d75dc42dcf9
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-08-16 16:42:02 -0500

    add repsonsive to card component and loading component

--- components/ListItems.tsx
+++ components/ListItems.tsx
@@ -8,9 +8,13 @@ type ListItemsProps = {
 
 const ListItems = ({ items }: ListItemsProps) => {
   return (
-    <Grid container spacing={2}>
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 1, sm: 8, md: 12 }}
+    >
       {items.map(({ id, name, price, image }) => (
-        <Grid item xs={4} key={id}>
+        <Grid item xs={2} sm={4} md={5} key={id}>
           <CardItem id={id} name={name} price={price} image={image} />
         </Grid>
       ))}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
 import useItems from "@hooks/useItems";
+import Loading from "@components/Loading";
 import ListItems from "@components/ListItems";
+import { useEffect } from "react";
 
 const Home = () => {
   const { items } = useItems();
-  return (
-    <div>
-      <h1>Avocados</h1>
-      <ListItems items={items} />
-    </div>
-  );
+  const loadComponent = () => {
+    if (items.length === 0) return <Loading />;
+    return (
+      <div>
+        <h1>Avocados</h1>
+        <ListItems items={items} />
+      </div>
+    );
+  };
+  return <>{loadComponent()}</>;
 };
 
 export default Home;
